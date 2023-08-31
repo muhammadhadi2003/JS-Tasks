dbb42debc51d6c74ec85790319959dcdb99862de
Muhammad Hadi <112942620+muhammadhadi2003@users.noreply.github.com>
2023-08-31 23:31:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Chapter 1
+
+// 1) alert("Hello User")
+// 2) alert("Error! Please Enter a Valid Passowrd")
+
+// 3) alert("Welcome to JS Land.... \nHappy Coding!")
+
+/*4) alert("Welcome to JS Land")
+alert("Happy Coding")*/
+
+// Chapter 2
+
+// 1) var username;
+
+/* 2) var myname;
+myname="Muhammad Hadi"*/
+
+/* 3) var message;
+message="Hello World";
+alert(message)*/
+
+/* 4) var sname="Jhone Doe";
+var age="15 years old";
+var course="Certified Mobile Application Development";
+alert(sname)
+alert(age)
+alert(course)*/
+
+/* 5) var pizza;
+pizza="PIZZA \nPIZZ \nPIZ \nPI \nP "
+alert(pizza)*/
+
+/* 6) var email
+email="myemail@gmail.com";
+alert("My email is " + email)*/
+
+
+/* 7) var book="A Smarter Way to Learn Javascript"
+alert("I am trying to learn from a book "+book)*/
+
+
+/* 8) document.write("Yah! I can write HTML content through JS")*/
+
+
+/* 9) var msg;
+msg="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+document.write(msg)
+alert(msg)*/
